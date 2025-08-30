70a8bae6edd4c63ddc19e61744135165a56baef6
kapumota <121532729+kapumota@users.noreply.github.com>
2025-08-30 12:23:58 -0500

    test_hello.py

--- labs/Laboratorio2/tests/test_hello.py
+++ labs/Laboratorio2/tests/test_hello.py
@@ -3,7 +3,7 @@ from src.hello import greet
 
 class TestGreet(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("Alice"), "Hello, Alice!")
+        self.assertEqual(greet("Paulette"), "Hello, Paulette!")
 
 if __name__ == "__main__":
     unittest.main()
