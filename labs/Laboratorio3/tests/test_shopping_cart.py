aa83452cf1df0df4b3c5aec07f1f30223320adcf
kapumota <121532729+kapumota@users.noreply.github.com>
2025-09-18 12:53:51 -0500

    test_shopping_cart.py

--- labs/Laboratorio3/tests/test_shopping_cart.py
+++ labs/Laboratorio3/tests/test_shopping_cart.py
@@ -2,8 +2,6 @@ from unittest.mock import Mock
 import pytest
 from src.shopping_cart import ShoppingCart
 
-
-
 def test_add_item():
     cart = ShoppingCart()
     cart.add_item("apple", 2, 0.5)  # nombre, cantidad, precio unitario
